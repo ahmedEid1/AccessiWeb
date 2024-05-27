8fcb2c0a34c62e1ee9f1f021c0ad6563e7decacd
Olumide Oluwole <oluwoleit@gmail.com>
2024-05-27 02:59:42 +0200

    remove unused imports

--- src/Layout/Dashboard/DashboardLayout.tsx
+++ src/Layout/Dashboard/DashboardLayout.tsx
@@ -1,6 +1,5 @@
 import NavBar from "@components/NavBar";
 import { Box } from "@mui/material";
-import React from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer, Zoom } from "react-toastify";
 
