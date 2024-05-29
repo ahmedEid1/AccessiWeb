5c75d5fc0a2358c28c40091486bd4d6fcbb53548
ahmedEid1 <ahmedeid6422@gmail.com>
2024-05-29 13:51:36 +0200

    make navbar responsive on mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,65 +1,86 @@
 import { Box, Link, Tooltip } from "@mui/material";
 import logo from "assets/accessiweb-two.png";
 import { NavLink } from "react-router-dom";
-import LogoutIcon from "@mui/icons-material/Logout";
+import { Logout, Menu, Close } from "@mui/icons-material";
 import { Button } from "@/components/ui/button";
 import { useAuthContext } from "provider/useAuth";
 import "./navbar.module.css";
+import { useState } from "react";
 
+interface INavItem {
+  title: string;
+  url: string;
+}
+const navItems: INavItem[] = [
+  {
+    title: "Home",
+    url: "home",
+  },
+  { title: "History", url: "history" },
+  { title: "Profile", url: "profile" },
+];
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const { logout } = useAuthContext();
 
   const handleLogout = () => {
     logout();
   };
+
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleCloseMenuOnMobile = () => {
+    if (window.innerWidth <= 800) setShowMenu(false);
+  };
+
   return (
     <Box className="fixed top-0 left-0 right-0 z-50 bg-white">
-      <nav className="flex text-center items-center p-4 border inline-block ">
-        <Box className="w-1/3 inline-block  ">
+      <nav className="flex justify-between items-center p-4 border inline-block  ">
+        <Box className=" w-1/3 ">
           <Link href="/dashboard">
             <img src={logo} width={200} />
           </Link>
         </Box>
 
-        <Box className="flex justify-center items-center gap-4 flex-grow md:w-1/3 inline-block ">
-          <NavLink
-            to="home"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-[#5454F6] text-white px-4 py-2 p rounded-md"
-                : "hover:underline hover:underline-offset-8"
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="history"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-[#5454F6] text-white px-4 py-2 p rounded-md"
-                : "hover:underline hover:underline-offset-8"
-            }
-          >
-            History
-          </NavLink>
-          {/* <NavLink
-            to="profile"
-            className={({ isActive }) =>
-              isActive
-                ? "bg-[#5454F6] text-white px-4 py-2 p rounded-md"
-                : "hover:underline hover:underline-offset-8"
-            }
-          >
-            Profile
-          </NavLink> */}
+        <Box className={`menus ${showMenu ? "show_menu" : ""}`}>
+          <ul className="menus__list">
+            {navItems.map((item) => (
+              <li key={item.title}>
+                <NavLink
+                  to={item.url}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-[#5454F6] text-white text-bold  px-4 py-2 rounded-md"
+                      : "hover:underline hover:underline-offset-8"
+                  }
+                  onClick={handleCloseMenuOnMobile}
+                >
+                  {item.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+
+          <Box className="">
+            <Tooltip title="Logout">
+              <Button onClick={handleLogout}>
+                Signout
+                <Logout className="ml-2" />
+              </Button>
+            </Tooltip>
+          </Box>
+
+          <Box className="menus__close" onClick={handleToggleMenu}>
+            <Tooltip title="close">
+              <Close className="cursor-pointer" />
+            </Tooltip>
+          </Box>
         </Box>
 
-        <Box className="md:w-1/3  justify-self-end text-right">
-          <Tooltip title="Logout">
-            <Button onClick={handleLogout}>
-              <LogoutIcon />
-            </Button>
-          </Tooltip>
+        <Box className="menus__hamburger" onClick={handleToggleMenu}>
+          <Menu className="cursor-pointer" />
         </Box>
       </nav>
     </Box>
